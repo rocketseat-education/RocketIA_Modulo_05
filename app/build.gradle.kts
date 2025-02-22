735db729ef3c2db63d99277efb69283af6151681
Egito Filho <egitofilho4@gmail.com>
2025-02-22 09:26:52 -0300

    05_14_CONSTRUINDO_AS_TELAS_DO_APLICATIVO_ROCKETIA_PARTE_01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 
 dependencies {
     implementation(libs.material)
+    implementation(libs.lottie)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
