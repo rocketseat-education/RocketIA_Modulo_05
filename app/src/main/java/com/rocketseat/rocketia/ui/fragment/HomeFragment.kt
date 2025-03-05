e7f23a273f0bf3d13f5e96f201e5f2e1ccb71616
Egito Filho <egitofilho4@gmail.com>
2025-03-05 20:56:51 -0300

    05_36_CONSTRUINDO_A_CAMADA_DE_UI_CONFIGURANDO_A_LOGICA_DE_NAVEGAÇÃO_DA_TELA_DE_HOME

--- app/src/main/java/com/rocketseat/rocketia/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/rocketseat/rocketia/ui/fragment/HomeFragment.kt
@@ -5,15 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.rocketseat.rocketia.R
+import com.rocketseat.rocketia.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.fcvHomeContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvHomeMenu.setupWithNavController(navController)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
