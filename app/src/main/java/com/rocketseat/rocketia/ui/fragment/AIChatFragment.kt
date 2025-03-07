c1b06ed4666360b3c46303d5dbd13e67c67e6666
Egito Filho <egitofilho4@gmail.com>
2025-03-07 20:25:13 -0300

    05_38_CONSTRUINDO_A_CAMADA_DE_UI_CONTINUANDO_A_LOGICA_DA_TELA_DE_CHAT_COM_A_AI

--- app/src/main/java/com/rocketseat/rocketia/ui/fragment/AIChatFragment.kt
+++ app/src/main/java/com/rocketseat/rocketia/ui/fragment/AIChatFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import android.widget.PopupMenu
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.findNavController
 import com.rocketseat.rocketia.databinding.FragmentAiChatBinding
 import com.rocketseat.rocketia.ui.viewmodel.AIChatViewModel
 import kotlinx.coroutines.launch
@@ -38,6 +40,15 @@ class AIChatFragment : Fragment() {
         setupObservers()
 
         with(binding) {
+            val userSettingsPopMenu = PopupMenu(
+                requireContext(),
+                ibUserSettings,
+            )
+            userSettingsPopMenu.setupUserSettingsPopMenu()
+            ibUserSettings.setOnClickListener {
+                userSettingsPopMenu.show()
+            }
+
             tietAIQuestion.doOnTextChanged { _, _, _, _, ->
                 if(tilAIQuestion.error != null)
                     tilAIQuestion.error = null
@@ -84,6 +95,23 @@ class AIChatFragment : Fragment() {
                         ).show()
                     }
                 }
+            }
+        }
+    }
+
+    private fun PopupMenu.setupUserSettingsPopMenu() {
+        this.menuInflater.inflate(R.menu.user_settings_menu, this.menu)
+
+        this.setOnMenuItemClickListener { itemMenu ->
+            when (itemMenu.itemId) {
+                R.id.action_change_stack -> {
+                    requireActivity().findNavController(R.id.fcvMainContainer).navigate(
+                        R.id.action_homeFragment_to_chooseStackFragment
+                    )
+                    true
+                }
+
+                else -> false
             }
         }
     }
