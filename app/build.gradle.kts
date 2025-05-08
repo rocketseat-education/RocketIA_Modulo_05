98b010a8ba3d0d066eddc1d427f2527daa343f72
Egito Filho <egitofilho4@gmail.com>
2025-05-08 19:14:07 -0300

    06_18_A_INTEROPERABILIDADE_ENTRE_VIEWS_EM_XML_E_COMPOSE_PARTE_01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.lottie)
     implementation(libs.bundles.navigation)
     implementation(libs.bundles.room)
+    implementation(libs.androidx.ui.android)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.koin.android)
