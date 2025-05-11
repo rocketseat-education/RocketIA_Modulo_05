fb2b10d7208a8798fb7fb7aa004f4c7cb7fdda8e
Egito Filho <egitofilho4@gmail.com>
2025-05-11 20:13:35 -0300

    06_20_A_INTEROPERABILIDADE_ENTRE_VIEWS_EM_XML_E_COMPOSE_PARTE_03

---
+++ app/src/main/java/com/rocketseat/rocketia/ui/compose/ChooseStackScreen.kt
@@ -0,0 +1,77 @@
+package com.rocketseat.rocketia.ui.compose
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.findNavController
+import com.google.android.material.chip.Chip
+import com.rocketseat.rocketia.R
+import com.rocketseat.rocketia.databinding.FragmentChooseStackBinding
+import com.rocketseat.rocketia.ui.event.ChooseStackUiEvent
+import com.rocketseat.rocketia.ui.viewmodel.ChooseStackViewModel
+import androidx.compose.runtime.getValue
+
+@Composable
+fun ChooseStackScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ChooseStackViewModel
+) {
+    val selectedChipId by viewModel.selectedStackChipId.collectAsStateWithLifecycle()
+    val isConfirmedNewStack by viewModel.isConfirmedNewStack.collectAsStateWithLifecycle()
+
+    AndroidViewBinding(
+        modifier = modifier.fillMaxSize(),
+        factory = FragmentChooseStackBinding::inflate
+    ) {
+        this.setupStackChips(
+            onStackChipClick = {
+                chip, chipId ->
+                viewModel.onEvent(
+                    event = ChooseStackUiEvent.SelectStack(
+                        selectedStackName = chip.text.toString(),
+                        selectedStackChipId = chipId
+                    )
+                )
+            }
+        )
+        btnChooseStackConfirm.setOnClickListener {
+            root.findNavController().navigate(R.id.action_chooseStackFragment_to_homeFragment)
+        }
+
+        btnChooseStackConfirm.isEnabled = isConfirmedNewStack
+
+        selectedChipId?.let { selectedStackChipId ->
+            this.changeSelectedStack(selectedStackChipId = selectedStackChipId)
+        }
+    }
+}
+
+private fun FragmentChooseStackBinding.setupStackChips(
+    onStackChipClick: (Chip, Int) -> Unit
+) {
+    flwChooseStackOptions.referencedIds.forEach { stackChipId ->
+        val stackChip = root.findViewById<Chip>(stackChipId)
+
+        stackChip.setOnClickListener {
+            onStackChipClick(stackChip, stackChipId)
+        }
+    }
+}
+
+private fun FragmentChooseStackBinding.changeSelectedStack(selectedStackChipId: Int) {
+    flwChooseStackOptions.referencedIds.forEach { stackChipId ->
+        val stackChip = root.findViewById<Chip>(stackChipId)
+
+        stackChip?.apply {
+            setChipStrokeColorResource(
+                if(stackChip.id == selectedStackChipId)
+                    R.color.white
+                else
+                    R.color.border_default
+            )
+            isChecked = stackChip.id == selectedStackChipId
+        }
+    }
+}
---
+++ app/src/main/java/com/rocketseat/rocketia/ui/fragment/ChooseStackComposeFragment.kt
@@ -0,0 +1,31 @@
+package com.rocketseat.rocketia.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.rocketseat.rocketia.ui.compose.ChooseStackScreen
+import com.rocketseat.rocketia.ui.viewmodel.ChooseStackViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.getValue
+
+
+class ChooseStackComposeFragment : Fragment() {
+
+    private val viewModel: ChooseStackViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(context = requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                ChooseStackScreen(viewModel = viewModel)
+            }
+        }
+    }
+}
