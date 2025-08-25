9eb2527799f3453d57ab9d58d1eb527fc11ad8d7
J Egito GS Filho <egitofilho4@gmail.com>
2025-08-24 21:36:50 -0300

    07_16_IMPLEMENTANDO_TESTES_PARA_A_CAMADA_DE_DADOS_MAPPER_PARTE_02

---
+++ app/src/test/java/com/rocketseat/rocketia/data/AIChatTextStub.kt
@@ -0,0 +1,12 @@
+package com.rocketseat.rocketia.data
+
+import com.rocketseat.rocketia.data.local.database.AIChatTextEntity
+import com.rocketseat.rocketia.domain.model.AIChatTextType
+
+fun createAIChatTextEntityStub(from: AIChatTextType?, text: String = ""): AIChatTextEntity =
+    AIChatTextEntity(
+        from = from?.name ?: "UNKNOWN",
+        stack = "stack",
+        text = text,
+        dateTime = 0L
+    )
--- app/src/test/java/com/rocketseat/rocketia/data/mapper/AiChatHistoryMapperTest.kt
+++ app/src/test/java/com/rocketseat/rocketia/data/mapper/AiChatHistoryMapperTest.kt
@@ -1,5 +1,12 @@
 package com.rocketseat.rocketia.data.mapper
 
+import com.rocketseat.rocketia.data.createAIChatTextEntityStub
+import com.rocketseat.rocketia.domain.model.AIChatText
+import com.rocketseat.rocketia.domain.model.AIChatTextType
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import org.junit.Assert.assertThrows
+
 import org.junit.Test
 
 class AiChatHistoryMapperTest {
@@ -7,7 +14,40 @@ class AiChatHistoryMapperTest {
     @Test
     fun `GIVEN AiChatHistoryEntity from USER QUESTION type WHEN toDomain is called THEN should convert to AIChatText from USER QUESTION`() {
         // GIVEN (dado que...)
+        val userQuestionEntity = createAIChatTextEntityStub(from = AIChatTextType.USER_QUESTION)
+
         // WHEN (quando...)
+        val result = userQuestionEntity.toDomain()
+
         // THEN (então...)
+        assertTrue(result is AIChatText.UserQuestion)
+        assertEquals(userQuestionEntity.text, (result as AIChatText.UserQuestion).question)
+    }
+
+    @Test
+    fun `GIVEN AiChatHistoryEntity from AI ANSWER type WHEN toDomain is called THEN should convert to AIChatText from AI ANSWER`() {
+        // GIVEN (dado que...)
+        val aiAnswerEntity = createAIChatTextEntityStub(from = AIChatTextType.AI_ANSWER)
+
+        // WHEN (quando...)
+        val result = aiAnswerEntity.toDomain()
+
+        // THEN (então...)
+        assertTrue(result is AIChatText.AIAnswer)
+        assertEquals(aiAnswerEntity.text, (result as AIChatText.AIAnswer).answer)
+    }
+
+    @Test
+    fun `GIVEN AiChatHistoryEntity from UNKNOWN type WHEN toDomain is called THEN should trigger an exception`() {
+        // GIVEN (dado que...)
+        val unknownEntity = createAIChatTextEntityStub(from = null)
+
+        // WHEN (quando...)
+        val result = assertThrows(IllegalArgumentException::class.java) {
+            unknownEntity.toDomain()
+        }
+
+        // THEN (então...)
+        assertEquals("Invalid from value: UNKNOWN", result.message)
     }
 }
