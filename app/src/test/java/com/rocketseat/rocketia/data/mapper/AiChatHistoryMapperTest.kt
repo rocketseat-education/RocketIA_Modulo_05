b076ed126e2e31049746d7ac0b21c637ba348ece
J Egito GS Filho <egitofilho4@gmail.com>
2025-08-24 21:02:55 -0300

    07_15_IMPLEMENTANDO_TESTES_PARA_A_CAMADA_DE_DADOS_MAPPER_PARTE_01

---
+++ app/src/test/java/com/rocketseat/rocketia/data/mapper/AiChatHistoryMapperTest.kt
@@ -0,0 +1,13 @@
+package com.rocketseat.rocketia.data.mapper
+
+import org.junit.Test
+
+class AiChatHistoryMapperTest {
+
+    @Test
+    fun `GIVEN AiChatHistoryEntity from USER QUESTION type WHEN toDomain is called THEN should convert to AIChatText from USER QUESTION`() {
+        // GIVEN (dado que...)
+        // WHEN (quando...)
+        // THEN (então...)
+    }
+}
