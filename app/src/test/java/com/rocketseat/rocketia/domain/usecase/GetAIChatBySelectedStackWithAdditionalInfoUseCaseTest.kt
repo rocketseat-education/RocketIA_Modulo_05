ca320edeaa1e21fe54503c982de769eac2665984
J Egito GS Filho <egitofilho4@gmail.com>
2025-08-30 14:01:50 -0300

    07_25_IMPLEMENTANDO_TESTES_PARA_A_CAMADA_DE_DOMINIO_USE_CASE_PARTE_02

--- app/src/test/java/com/rocketseat/rocketia/domain/usecase/GetAIChatBySelectedStackWithAdditionalInfoUseCaseTest.kt
+++ app/src/test/java/com/rocketseat/rocketia/domain/usecase/GetAIChatBySelectedStackWithAdditionalInfoUseCaseTest.kt
@@ -1,9 +1,18 @@
 package com.rocketseat.rocketia.domain.usecase
 
+import com.rocketseat.rocketia.data.createAIChatTextEntityStub
+import com.rocketseat.rocketia.data.mapper.toDomain
+import com.rocketseat.rocketia.domain.model.AIChatTextType
 import com.rocketseat.rocketia.domain.repository.AIChatRepository
+import com.rocketseat.rocketia.domain.util.formatDatetime
+import io.mockk.coEvery
 import io.mockk.mockk
+import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import java.util.Locale
 
 class GetAIChatBySelectedStackWithAdditionalInfoUseCaseTest {
 
@@ -21,23 +30,106 @@ class GetAIChatBySelectedStackWithAdditionalInfoUseCaseTest {
     }
 
     @Test
-    fun `GIVEN additional info not received WHEN invoked THEN should return same chat conversation`() {
-        // GIVEN
-        // WHEN
-        // THEN
-    }
+    fun `GIVEN additional info not received WHEN invoked THEN should return same chat conversation`() =
+        runTest {
+            // GIVEN
+            val dummyStack = "Kotlin"
+            val dummyChatConversation = listOf(
+                createAIChatTextEntityStub(
+                    from = AIChatTextType.AI_ANSWER,
+                    stack = dummyStack,
+                    text = "answer"
+                )
+            ).toDomain()
+
+            val expectedChatTextWithoutAdditionalInfo = listOf(
+                AIChatTextWithAdditionalInfo(
+                    datetime = null,
+                    chatText = dummyChatConversation[0],
+                    additionalInfo = null
+                )
+            )
+
+            coEvery { aiChatRepository.getAIChatByStack(stack = any()) } returns dummyChatConversation
+            coEvery { aiAdditionalInfoRepository.getAdditionalInfo(answer = any()) } returns null
+
+            // WHEN
+            val result = useCase(stack = dummyStack)
+            val firstItem = result[0]
+
+            // THEN
+            assertEquals(1, result.size)
+            assertEquals(expectedChatTextWithoutAdditionalInfo[0], firstItem)
+        }
 
     @Test
-    fun `GIVEN additional info received WHEN invoked THEN should return update chat conversation`() {
-        // GIVEN
-        // WHEN
-        // THEN
-    }
+    fun `GIVEN additional info received WHEN invoked THEN should return update chat conversation`() =
+        runTest {
+            // GIVEN
+            val dummyStack = "Kotlin"
+            val dummyChatConversation = listOf(
+                createAIChatTextEntityStub(
+                    from = AIChatTextType.AI_ANSWER,
+                    stack = dummyStack,
+                    text = "answer"
+                )
+            ).toDomain()
+
+            val dummyLocale = Locale.forLanguageTag("pt-BR")
+            val expectedAdditionalInfo = AIAdditionalInfo(
+                datetime = 1756295438711L,
+                text = "additional info"
+            )
+
+            val expectedChatTextWithAdditionalInfo = AIChatTextWithAdditionalInfo(
+                datetime = expectedAdditionalInfo.datetime.formatDatetime(locale = dummyLocale),
+                chatText = dummyChatConversation.first(),
+                additionalInfo = expectedAdditionalInfo
+            )
+
+            coEvery { aiChatRepository.getAIChatByStack(stack = any()) } returns dummyChatConversation
+            coEvery { aiAdditionalInfoRepository.getAdditionalInfo(answer = any()) } returns expectedAdditionalInfo
+
+            // WHEN
+            val result = useCase(stack = dummyStack)
+            val firstItem = result[0]
+
+            // THEN
+            assertEquals(1, result.size)
+            assertNotNull(expectedChatTextWithAdditionalInfo.additionalInfo)
+            assertEquals(expectedChatTextWithAdditionalInfo, firstItem)
+        }
 
     @Test
-    fun `GIVEN just user question chat text WHEN invoked THEN should return same chat conversation`() {
-        // GIVEN
-        // WHEN
-        // THEN
-    }
+    fun `GIVEN just user question chat text WHEN invoked THEN should return same chat conversation`() =
+        runTest {
+            // GIVEN
+            val dummyStack = "Kotlin"
+            val dummyChatConversation = listOf(
+                createAIChatTextEntityStub(
+                    from = AIChatTextType.USER_QUESTION,
+                    stack = dummyStack,
+                    text = "answer"
+                )
+            ).toDomain()
+
+            val expectedChatTextWithoutAdditionalInfo = listOf(
+                AIChatTextWithAdditionalInfo(
+                    datetime = null,
+                    chatText = dummyChatConversation[0],
+                    additionalInfo = null
+                )
+            )
+
+            coEvery { aiChatRepository.getAIChatByStack(stack = any()) } returns dummyChatConversation
+            coEvery { aiAdditionalInfoRepository.getAdditionalInfo(answer = any()) } returns null
+
+            // WHEN
+            val result = useCase(stack = dummyStack)
+            val firstItem = result[0]
+
+            // THEN
+            assertEquals(1, result.size)
+            assertEquals(expectedChatTextWithoutAdditionalInfo[0], firstItem)
+        }
 }
